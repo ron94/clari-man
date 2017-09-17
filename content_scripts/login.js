a6cfc5fc7f58db1295c4aa72c8dd5e1874c3228c
Ron <ron941020@gmail.com>
2017-09-18 00:48:26 +0300

    minor fixes

--- content_scripts/login.js
+++ content_scripts/login.js
@@ -1,9 +1,12 @@
-let result = browser.storage.local.get(["p_user", "p_sisma", "p_mis_student"]);
-result.then(function (result) {
-    if(result.p_user && result.p_sisma && result.p_mis_student) {
-        document.form1.p_user.value = result.p_user;
-        document.form1.p_sisma.value = result.p_sisma;
-        document.form1.p_mis_student.value = result.p_mis_student;
-        document.form1.submit();
-    }
-});
+debugger;
+if (document.form1) {
+    let result = browser.storage.local.get(["p_user", "p_sisma", "p_mis_student"]);
+    result.then(function (result) {
+        if (result.p_user && result.p_sisma && result.p_mis_student) {
+            document.form1.p_user.value = result.p_user;
+            document.form1.p_sisma.value = result.p_sisma;
+            document.form1.p_mis_student.value = result.p_mis_student;
+            document.form1.submit();
+        }
+    });
+}
